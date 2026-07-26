4d274fa12ee67cf8878e0f6dcf0d76a6aad9748a
Chun-Yu Wang <wicanr2@gmail.com>
2026-07-27 05:52:00 +0800

    feat: add cause light wounds combat spell

--- internal/game/spells.go
+++ internal/game/spells.go
@@ -9,9 +9,10 @@ import (
 )
 
 const (
-	BlessSpellID        uint8 = 1
-	CurseSpellID        uint8 = 2
-	MagicMissileSpellID uint8 = 7
+	BlessSpellID            uint8 = 1
+	CurseSpellID            uint8 = 2
+	CauseLightWoundsSpellID uint8 = 4
+	MagicMissileSpellID     uint8 = 7
 )
 
 // firstLevelSpellKeys contains only spell names whose class table order is
