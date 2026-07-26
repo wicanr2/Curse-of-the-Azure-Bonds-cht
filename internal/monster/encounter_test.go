f7ec7ca4bf725db9e6148d4aa5f9ff6fbddc86ba
Chun-Yu Wang <wicanr2@gmail.com>
2026-07-26 22:58:53 +0800

    feat: build enemies from ECL encounter

---
+++ internal/monster/encounter_test.go
@@ -0,0 +1,24 @@
+package monster
+
+import (
+	"testing"
+
+	"github.com/wicanr2/Curse-of-the-Azure-Bonds-cht/internal/ecl"
+)
+
+func TestBuildEnemiesJoinsSpawnAndRecords(t *testing.T) {
+	record := Record{Name: "Goblin", HitPoints: 5, MaxHitPoints: 5, ArmorClass: 10, DamageDiceCount: 1, DamageDiceSides: 6}
+	enemies, err := BuildEnemies([]ecl.MonsterSpawn{{MonsterID: 7, Count: 2}}, map[uint8]Record{7: record})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(enemies) != 2 || enemies[0].ID != "monster-07-1" || enemies[1].Name != "Goblin" || enemies[1].Side != 1 {
+		t.Fatalf("enemies=%#v", enemies)
+	}
+}
+
+func TestBuildEnemiesRejectsMissingRecord(t *testing.T) {
+	if _, err := BuildEnemies([]ecl.MonsterSpawn{{MonsterID: 7, Count: 1}}, nil); err == nil {
+		t.Fatal("expected missing record error")
+	}
+}
