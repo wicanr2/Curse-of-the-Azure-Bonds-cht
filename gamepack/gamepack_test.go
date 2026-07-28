3cc2aa4f26049efb356df6b04c647937f72d4347
Chun-Yu Wang <cy.wang@coretronic.com>
2026-07-28 19:21:04 +0800

    refactor: drive first-person maps from shared engine

--- gamepack/gamepack_test.go
+++ gamepack/gamepack_test.go
@@ -13,6 +13,12 @@ func TestEmbeddedPackValidatesAndOwnsZhentilText(t *testing.T) {
 	if pack.ID != "curse-of-the-azure-bonds.pit-of-moander" {
 		t.Fatalf("pack id=%q", pack.ID)
 	}
+	if len(pack.Maps) != 1 || pack.Maps[0].ID != "zhentil-keep.inner-city" ||
+		pack.Maps[0].AreaID != 4 || pack.Maps[0].GeometryBlock != 0x20 ||
+		pack.Maps[0].Spawn == nil || pack.Maps[0].Spawn.X != 2 ||
+		pack.Maps[0].Spawn.Y != 0 || pack.Maps[0].Spawn.Direction != 4 {
+		t.Fatalf("Zhentil Keep map definition=%+v", pack.Maps)
+	}
 	result := pack.MatchText([]string{
 		"YOU ARE CONFRONTED BY A PATROL FROM ZHENTIL KEEP.",
 		"NOTING THE SIGILS ON YOUR ARMS, THEY LET YOU PASS.",
--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ toolchain go1.24.13
 
 require (
 	github.com/hajimehoshi/ebiten/v2 v2.6.6
-	github.com/wicanr2/golden-box-remake-engine v0.0.0-20260728105151-83063cde4894
+	github.com/wicanr2/golden-box-remake-engine v0.0.0-20260728111811-72aa75802e50
 	golang.org/x/image v0.12.0
 )
 
